862122021089dfb1eddf0788f88963655a6c29ef
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2017-06-13 13:12:50 +0300

    Refactored array creation

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -2,13 +2,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   conversationKey: 'one',
   items: Ember.computed(function() {
-    let output = [];
-    for (let i = 0; i < 10; i++) {
-      output.push({
-        id: i
-      });
-    }
-    return output;
+    return Array(10).fill(null).map((_, id) => ({id}));
   }),
   actions: {
     changeConversation(conversationKey) {
